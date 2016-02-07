bb1696c21dd14e00c916073e0c7e9593cb7ab3bb
franciscog <frankjg@gmail.com>
2016-02-06 23:24:31 -0500

    still planning things out

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-nothing to see here yet.
+## nothing to see here yet.
 
-Maybe I won't use jQuery, we'll see. 
+dependency: jQuery 
+
+concept:
+
+A Portable library that's intended to handle the intricacies of autocomplete filtering from, and to, a text input.  Most other libraries I found would only begin filtering at the first character in an input box.  I would like to be able to filter at any point in the input box based on multiple criteria (like how Slack does it).
+
+I started developing it as part of the [DubX](https://github.com/sinfulBA/DubX-Script) plugin/extension for [Dubtrack.fm](http://dubtrack.fm) to autocomplete emojis and mentions but decided to separate it so I can improve and test
--- dist/multi-autocomplete.js
+++ dist/multi-autocomplete.js
@@ -1,36 +1,63 @@
 /* global jQuery */
-(function(){
+(function($){
   'use strict';
-/* 
-  Multi Autocomplete will be broken up into these modular sections
-  - plugin start and options intake
-  - input box event handling
-  - sifting through the datasets for matches, return a single array
-  - take single array and filter again based on 2nd criteria (optional)
-  - display the array of data into the preview popup box
-  - handling key events for popup box for selection 
-  - replacing text in input box with selected text
-*/
-
 
 function MultiComplete(options){
   
-  /* 
-    options needed:
+  /**
+   * User defined options, all required
+   */
   
-    - id/class of the input box
-    - array of datasets.  an array of arrays (or just 1) [myarray] or [array1, array2]
-    - regex? for matching??
-    - id/class of the container for the preview list. 
+  this.input = $(options.input);
+  if (!options.input || !this.input.length) {
+    this.warn("input not set or doesn't exist");
+    return;
+  }
 
-    callback functions:
-    - run on each matched item of the array before sending to preview
+  this.previewContainer = $(options.input);
+  if (!options.previewContainer || !this.previewContainer.length) {
+    this.warn("preview container not set or doesn't exist");
+    return;
+  }
 
-  
-  */
+  if (!options.datasets) {
+    this.warn("datasets missing");
+    return;
+  }
+  this.datasets = options.datasets;
+
+  this.keycodes = {
+    up : 38, 
+    down : 40, 
+    left : 37,
+    right : 39, 
+    enter : 13, 
+    esc : 27,
+    tab : 9,
+    shift : 16
+  };
 }
 
+MultiComplete.prototype = {
+  log: function(stuff){
+    if (console && console.log) { console.log(stuff) ;}
+  }, 
+  warn: function(stuff){
+    if (console && console.warn) { console.warn("mutli-autocomplete:",stuff) ;}
+  }
+}
+
+/*
+  on each keyup:
+
+  check if character is one of the initiators and that previous character is a space, 
+    if so start saving from that position until the next space or end of line
+
+    when that saved string gets to a certain character length, start filtering
+
+    
+
+ */
 
 
-
-})();
+})(jQuery);

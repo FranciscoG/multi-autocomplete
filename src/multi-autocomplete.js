688640797a975a9a082a5cfd031267723fd50135
franciscog <frankjg@gmail.com>
2016-02-09 00:21:32 -0600

    working on key bindings

--- src/multi-autocomplete.js
+++ src/multi-autocomplete.js
@@ -7,7 +7,7 @@ function MultiComplete(options){
   }
 
   /**
-   * User defined options, all required
+   * User defined options, required
    */
   
   this.input = options.input;
@@ -17,8 +17,9 @@ function MultiComplete(options){
     return;
   }
 
-  this.previewContainer = $(options.input);
-  if (!options.previewContainer || !this.previewContainer.length) {
+  this.output = options.output;
+  this.$output = $(options.output);
+  if (!options.output || !this.$output.length) {
     this.warn("preview container not set or doesn't exist");
     return;
   }
@@ -34,6 +35,11 @@ function MultiComplete(options){
   }
   this.markers = new RegExp("["+markers+"]", "i");
 
+  /**
+   * User defined options, optional (with defaults)
+   */
+  this.outputDom = options.outputDom || "li";
+  
   this.keycodes = {
     up    : 38, 
     down  : 40, 
@@ -46,6 +52,7 @@ function MultiComplete(options){
     del   : 8
   };
 
+  this.info = {};
   $('body').on('keyup', options.input, this.onKeyUp.bind(this));
 
 }
@@ -56,25 +63,24 @@ MultiComplete.prototype = {
     if (console && console.warn) { console.warn("mutli-autocomplete:",stuff) ;}
   },
 
-  goForward: function(arr, start) {
+  goForward: function(str, start) {
     // find the next space in the array starting at 
-    var returnIndex = arr.length
-    for (var i = start, len = arr.length; i < len; i++) {
-      if (arr[i] === " ") {
+    var returnIndex = strLen = str.length;
+    for (var i = start; i < strLen; i++) {
+      if (str[i] === " ") {
         returnIndex = i;
         break;
       }
     }
     return returnIndex;
-
   },
 
-  goBackward: function(arr, start) {
+  goBackward: function(str, start) {
     // find the next space in the array going backwards in the array
     var returnIndex = 0;
     for (var i = start - 1; i >= 0; i--) {
-      if (arr[i] === " ") {
-        returnIndex = i;
+      if (str[i] === " ") {
+        returnIndex = i + 1;
         break;
       }
     }
@@ -86,49 +92,105 @@ MultiComplete.prototype = {
 
     if (val) {
       var cursorPos = this.$input.get(0).selectionStart;
-      var valArr = val.split("");
-      var leftChar = valArr[cursorPos - 1];
+      var leftChar = val[cursorPos - 1];
       
       var startIndex;
       var endIndex;
 
-      if (valArr[cursorPos] === " ") {
+      if (val[cursorPos] === " ") {
         endIndex = cursorPos;
       } else {
-        endIndex = mc.goForward(valArr, cursorPos);
+        endIndex = this.goForward(val, cursorPos);
       }
 
       if (leftChar === " ") {
         startIndex = cursorPos;
       } else {
-        startIndex = mc.goBackward(valArr, cursorPos);
+        startIndex = this.goBackward(val, cursorPos);
       }
 
       var currentWord = val.substring(startIndex,endIndex);
+      this.info = {
+        start: startIndex, 
+        end: endIndex,
+        fullStr: currentWord,
+        filterStr : currentWord.substr(1),
+        val: val
+      };
 
+      var firstCharOfWord = currentWord.charAt(0);
       // console.log(startIndex,endIndex, currentWord);
-      if (this.markers.test(currentWord.charAt(0))) {
-        this.beginFiltering(currentWord.charAt(0), currentWord.substr(1));
+      if (this.markers.test(firstCharOfWord)) {
+        this.info.activeMarker = firstCharOfWord;
+        this.beginFiltering(firstCharOfWord, currentWord.substr(1));
+        this.bindPreview(evt.keyCode);
+      } else {
+        this.clearPreview();
       }
+
+      // console.log(this.info);
       return;
     }
+
+    if (!val || !this.info.activeMarker) {
+      this.clearPreview();
+    }
     
+  },
+
+  clearPreview: function() {
+    document.querySelector(this.output).innerHTML = "";
   },
 
   beginFiltering: function(marker, filterStr){
     if (!this.datasets[marker]) { return; }
 
     var dataToFilter = this.datasets[marker];
-    dataToFilter.forEach(function(el,i,r){
-      if (el.indexOf(filterStr) >= 0) {
-        console.log("found:", el);
-      }
+    var filteredData = dataToFilter.filter(function(el,i,r){
+      return el.indexOf(filterStr) >= 0;
     });
-    console.log("-------");
+    this.addToPreview(filteredData);
+  },
+
+  addToPreview: function(filteredData) {
+    var prev = document.querySelector(this.output);
+    this.clearPreview();
+    var itemDom = this.outputDom;
+
+    var tmpFrag = document.createDocumentFragment();
+    filteredData.forEach(function(el,i){
+      var item = document.createElement(itemDom);
+      item.textContent = el;
+      if (i === 0) { item.classList.add("active"); }
+      tmpFrag.appendChild(item);
+    });
+    prev.appendChild(tmpFrag);
+  },
+
+  bindPreview: function(keyCode) {
+    var $activeItem = this.$output.find('.active');
+    var newItem;
+
+    if (keyCode === this.keycodes.up) {
+      $activeItem.removeClass('active');
+      newItem = $activeItem.index() - 1;
+    } else if (keyCode === this.keycodes.down) {
+      $activeItem.removeClass('active');
+      newItem = $activeItem.index() + 1;
+    }
+
+    var childLen = this.$output.children().length - 1;
+    if (newItem < 0) {
+      newItem = childLen;
+    } else if (newItem >= childLen) {
+      newItem = 0;
+    }
+    console.log(newItem);
+    this.$output.children().eq(newItem).addClass('active');
   },
 
   replaceInPlace: function(str){
-    var val = this.$input.val();
+    var val = this.info.val;
     var newVal = val.slice(0, this.startPosition) +  str + val.slice(this.lastCursorPos, val.length - 1);
     console.log(newVal);
   }
@@ -138,19 +200,3 @@ MultiComplete.prototype = {
 if (typeof exports === "object") {
   module.exports = MultiComplete;
 }
-
-/*
-  ideas:
-  
-  1. when marker is detected, save each keystroke to a variable until a space is detected 
-
-  2. when marker is detected, save position index in string and then use substring to grab text using currect getSelelection index point
-  
-  issue:  If you started an unfinished item and went back to it, it doesn't detect it.  Have to add extra steps for this
-
-  3. split string separated by space into array.  Iterate over earch item looking for markers
-
-  issue: have to get position in string for future in-place replacement and that might be difficult especially if the same text occurs in multiple places in the string
-
-  4. split string into individual characters.  Starting at getSelection index, chech if index on each side (+1/-1) is a space.  if -1 is space, check for next space going forward (position direction).  Vice Versa if +1 is a space.  
- */
